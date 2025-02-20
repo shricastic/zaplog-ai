acbf3020d7d1fc39fbe57a0ec74401935a109ac5
Shri <shricse07@gmail.com>
2025-02-20 21:03:42 +0530

    fix(ui): landing page and publish page

--- frontend/src/feature/LandingPage/Hero.tsx
+++ frontend/src/feature/LandingPage/Hero.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 import Wrapper from '@/components/Wrapper'
 import React from 'react'
 import { Link } from 'react-router'
-import Dashboard from '@/assets/dashboard.svg'
+import Dashboard from '@/assets/dashboard.png'
 import SectionBadge from '@/components/SectionBadge'
 import Icons from '@/components/icons'
 import { features, perks, pricingCards, reviews } from '@/constants'
@@ -24,7 +24,7 @@ const Hero = () => {
     <section className="w-full relative flex items-center justify-center flex-col px-4 md:px-0 py-8">
       {/* hero */}
       <Wrapper>
-        <div className="absolute inset-0 dark:bg-[linear-gradient(to_right,#e2e8f0_1px,transparent_1px),linear-gradient(to_bottom,#e2e8f0_1px,transparent_1px)] bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] z[-10] h-[150vh]" />
+        <div className="absolute inset-0 dark:bg-[linear-gradient(to_right,#e2e8f0_1px,transparent_1px),linear-gradient(to_bottom,#e2e8f0_1px,transparent_1px)] bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem]  z[-10] h-[150vh]" />
 
         <Container>
           <div className="flex flex-col items-center justify-center py-20 h-full ">
--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,3 +1,6 @@
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { environment } from "@/env";
 import axios from "axios";
 import { useState } from "react";
@@ -46,63 +49,67 @@ export default function Publish() {
 
   return (
     <div>
-      <div className="min-h-screen bg-gray-100 p-4 flex items-center justify-center">
-        <div className="bg-white w-full max-w-4xl h-[36rem] rounded-lg shadow-lg overflow-hidden flex flex-col">
-          <div className="flex items-center p-4 border-b">
-            <button className="text-gray-900 hover:text-gray-600 transition-colors">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 4.5v15m7.5-7.5h-15"
-                />
-              </svg>
-            </button>
-            <input
-              type="text"
-              placeholder="Write a title for your blog post..."
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className="ml-4 text-2xl font-light text-gray-700 placeholder-gray-400 focus:outline-none flex-grow"
+      <div className="min-h-[90vh] p-4 flex items-center justify-center">
+        <div className="relative w-full max-w-4xl h-[36rem]  p-4 ">
+          <div className="absolute top-1/2 left-1/2 z-9 gradient w-3/4 -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
+          <Card className="relative w-full max-w-4xl h-[36rem]  p-4  flex flex-col z-10">
+            <div className="flex items-center p-4 ">
+              <button className="  ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                  />
+                </svg>
+              </button>
+              <Input
+                type="text"
+                placeholder="Write a title for your blog post..."
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="ml-4 text-2xl font-light   flex-grow "
+              />
+            </div>
+            <Textarea
+              placeholder="Write your blog post here... or put a title and hit generate with AI ✨ and let the AI write for you"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              className="flex-grow p-4  resize-none"
             />
-          </div>
-          <textarea
-            placeholder="Write your blog post here... or put a title and hit generate with AI ✨ and let the AI write for you"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            className="flex-grow p-4 text-gray-700 placeholder-gray-400 focus:outline-none resize-none"
-          />
-          <div className="flex justify-end p-4 ">
-            <button
-              onClick={generateBlog}
-              type="button"
-              className="text-white bg-violet-600 mr-5
-            hover:bg-violet-700 focus:outline-none focus:ring-4 focus:ring-violet-300 font-medium 
-            rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 shadow-lg transform hover:scale-105 
-            transition-all duration-200 hover:shadow-violet-300/50"
-            >
-              {" "}
-              Generate with AI ✨
-            </button>
+            <div className="flex justify-end p-4 ">
+              <button
+                onClick={generateBlog}
+                type="button"
+                className="text-white bg-violet-600 mr-5
+              hover:bg-violet-700 focus:outline-none focus:ring-4 focus:ring-violet-300 font-medium 
+              rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 shadow-lg transform hover:scale-105 
+              transition-all duration-200 hover:shadow-violet-300/50"
+              >
+                {" "}
+                Generate with AI ✨
+              </button>
 
-            <button
-              onClick={publishBlog}
-              type="button"
-              className="text-white bg-green-700 mr-5
-            hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium 
-            rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
-            >
-              Publish
-            </button>
-          </div>
+              <button
+                onClick={publishBlog}
+                type="button"
+                className="text-white bg-green-700 mr-5
+              hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium 
+              rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Publish
+              </button>
+            </div>
+          </Card>
         </div>
+
       </div>
     </div>
   );
